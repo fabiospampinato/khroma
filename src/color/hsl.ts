02c1635a245123a48375fe50351f8fb1ab00798c
John Letey <30328854+johnletey@users.noreply.github.com>
2019-09-27 16:47:06 +0100

    Convert everything to RGBA internally

--- src/color/hsl.ts
+++ src/color/hsl.ts
@@ -1,30 +1,38 @@
 
 /* IMPORT */
 
-import {HSLA} from '../types';
+import {RGBA} from '../types';
+import Abstract from './abstract';
+import Utils from '../utils';
 
 /* HSL */
 
-class HSL {
+class HSL extends Abstract {
 
   re = /(hsl|hsla)\(\s*(\d+?(?:\.\d+)?),\s*(\d+?(?:\.\d+)?),\s*(\d+?(?:\.\d+)?)\s*,?(\d+?(?:\.\d+)?)?\s*\)/i; //TODO: Support all possible formats
 
-  parse ( color: string ): HSLA | undefined {
+  parse ( color: string ): RGBA | undefined {
 
     const match = color.match ( this.re );
 
     if ( !match ) return;
 
-    return {
+    var hsl = {
       h: Number ( match[1] ),
       s: Number ( match[2] ),
       l: Number ( match[3] ),
       a: !match[4] ? 1 : Number ( match[4] )
     };
 
+    var rgba = Utils.hsla2rgba(hsl);
+
+    return rgba;
+
   }
 
-  output ( hsl: HSLA ): string {
+  output ( rgba: RGBA ): string {
+
+    var hsl = Utils.rgba2hsla(rgba);
 
     if ( hsl.a < 1 ) { // HSLA
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import { RGBA, HSLA } from "./types";
 
 /* UTILS */
 
@@ -58,6 +59,130 @@ const Utils = {
   hex2dec ( hex: string ): number {
 
     return parseInt ( hex, 16 );
+
+  },
+
+  rgba2hsla ( rgba: RGBA ): HSLA {
+
+    const red = rgba.r / 255;
+    const green = rgba.g / 255;
+    const blue = rgba.b / 255;
+
+    const max = Math.max(red, green, blue);
+    const min = Math.min(red, green, blue);
+    const lightness = (max + min) / 2;
+
+    if (max === min) {
+
+      // achromatic
+      if (rgba.a !== undefined) {
+
+        return {
+          h: 0,
+          s: 0,
+          l: lightness,
+          a: rgba.a,
+        }
+
+      } else {
+
+        return { h: 0, s: 0, l: lightness, a: 1 }
+
+      }
+
+    }
+
+    let hue;
+    const delta = max - min;
+    const saturation = lightness > 0.5 ? delta / ( 2 - max - min ) : delta / ( max + min );
+    switch ( max ) {
+
+      case red:
+        hue = ( green - blue ) / delta + ( green < blue ? 6 : 0 );
+        break
+      case green:
+        hue = ( blue - red ) / delta + 2;
+        break
+      default:
+        // blue case
+        hue = ( red - green ) / delta + 4;
+        break
+      
+    }
+
+    hue *= 60;
+    if (rgba.a !== undefined) {
+      return {
+        h: hue,
+        s: saturation,
+        l: lightness,
+        a: rgba.a,
+      };
+    }
+
+    return { h: hue, s: saturation, l: lightness, a: 1 };
+
+  },
+
+  hsla2rgba ( hsl: HSLA ): RGBA {
+
+    var hue = hsl.h;
+    var saturation = hsl.s;
+    var lightness = hsl.l;
+
+    if (saturation === 0) {
+      // achromatic
+      return { r: lightness, g: lightness, b: lightness, a: hsl.a | 1 };
+    }
+  
+    // formulae from https://en.wikipedia.org/wiki/HSL_and_HSV
+    const huePrime = ( ( ( hue % 360 ) + 360 ) % 360 ) / 60;
+    const chroma = ( 1 - Math.abs( 2 * lightness - 1 ) ) * saturation;
+    const secondComponent = chroma * ( 1 - Math.abs( ( huePrime % 2 ) - 1 ) );
+  
+    let red = 0;
+    let green = 0;
+    let blue = 0;
+  
+    if ( huePrime >= 0 && huePrime < 1 ) {
+
+      red = chroma;
+      green = secondComponent;
+
+    } else if ( huePrime >= 1 && huePrime < 2 ) {
+
+      red = secondComponent;
+      green = chroma;
+
+    } else if ( huePrime >= 2 && huePrime < 3 ) {
+
+      green = chroma;
+      blue = secondComponent;
+
+    } else if ( huePrime >= 3 && huePrime < 4 ) {
+
+      green = secondComponent;
+      blue = chroma;
+
+    } else if ( huePrime >= 4 && huePrime < 5 ) {
+
+      red = secondComponent;
+      blue = chroma;
+
+    } else if ( huePrime >= 5 && huePrime < 6 ) {
+
+      red = chroma;
+      blue = secondComponent;
+
+    }
+  
+    const lightnessModification = lightness - chroma / 2;
+    return {
+      r: red + lightnessModification,
+      g: green + lightnessModification,
+      b: blue + lightnessModification,
+      a: hsl.a | 1
+    };
 
   }
 
