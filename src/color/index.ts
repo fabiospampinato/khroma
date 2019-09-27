0f17e20b21ad4cb8a0a86f934cb2e377734b6627
John Letey <30328854+johnletey@users.noreply.github.com>
2019-09-27 11:31:59 +0100

    Remove irrelevent code

--- src/color/index.ts
+++ src/color/index.ts
@@ -30,12 +30,6 @@ const Color = {
 
     if ( rgba.a < 1 ) { // RGB
 
-      if ( rgba.a == 0 ) { // transparent
-
-        return `transparent`;
-        
-      }
-
       return RGB.output ( rgba );
 
     } else { // HEX
