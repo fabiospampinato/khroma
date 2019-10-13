efd7b51d4daadba0c19719adcd27ab85b1edf73f
Andrew P Maney <amaney@usc.edu>
2019-10-13 12:23:27 -0700

    Adds support for RGB colors
    Includes tests

--- src/color/rgb.ts
+++ src/color/rgb.ts
@@ -3,12 +3,13 @@
 
 import {RGBA} from '../types';
 import Abstract from './abstract';
+import Utils from '../utils';
 
 /* RGB */
 
 class RGB extends Abstract {
 
-  re = /rgba\(\s*(\d+?(?:\.\d+)?),\s*(\d+?(?:\.\d+)?),\s*(\d+?(?:\.\d+)?),\s*(\d+?(?:\.\d+)?)\s*\)/i; //TODO: Support all possible formats
+  re = /rgba?\(\s*(-?\d+(?:\.\d+)?%?)\s*(?:,|\s)\s*(-?\d+(?:\.\d+)?%?)\s*(?:,|\s)\s*(-?\d+(?:\.\d+)?%?)(?:\s*(?:,|\s)\s*(-?\d+(?:\.\d+)?%?))?\s*\)/i; //TODO: make less gross
 
   parse ( color: string ): RGBA | undefined {
 
@@ -16,11 +17,17 @@ class RGB extends Abstract {
 
     if ( !match ) return;
 
+    const [ , r, g, b, a ] = match as Array<string>;
+
+    const format = ( num: string ): number => Utils.clamp ( Utils.str2dec ( num ), 0, 255 );
+    
+    const formatAlpha = ( num: string ): number => Utils.clamp ( Number ( Utils.str2normDec ( num ) ), 0, 1 );
+
     return {
-      r: Number ( match[1] ),
-      g: Number ( match[2] ),
-      b: Number ( match[3] ),
-      a: Number ( match[4] )
+      r: format ( r ) ,
+      g: format ( g ) ,
+      b: format ( b ) ,
+      a: formatAlpha ( a || '1' )
     };
 
   }
--- test/color.js
+++ test/color.js
@@ -16,7 +16,7 @@ describe ( 'Color', it => {
     ];
 
     tests.forEach ( color => {
-      t.throws ( () => Color.parse ( color ) );
+      t.throws ( () => Color.parse ( color ), `Unsupported color format: "${color}"` );
     });
 
   });
@@ -30,6 +30,100 @@ describe ( 'Color', it => {
 
     tests.forEach ( ([ keyword, result ]) => {
       t.is ( Hex.output ( Color.parse ( keyword ) ), result );
+    });
+
+  });
+
+  /* RGB */
+
+  it ( 'parses RGB as numbers (bounded by 0 and 255)', t => {
+
+    const tests = [
+      ['rgb(1, 20, 255)', '#0114ff'],
+      ['rgb(1.99, 20.5, 255)', '#0215ff'],
+      ['rgb(300, 255, -100)', '#ffff00'],
+    ];
+
+    tests.forEach( ([ input, result ]) => {
+      t.is ( Hex.output ( Color.parse ( input ) ), result )
+    });
+
+  });
+
+  it ( 'parses RGB as percentages (bounded by 0 and 100)', t => {
+
+    const tests = [
+      ['rgb(10%, 20%, 30%)', '#1a334d'],
+      ['rgb(10.5%, 20.7%, 30%)', '#1b354d'],
+      ['rgb(100% 200% -30%)', '#ffff00'],
+    ];
+
+    tests.forEach( ([ input, result ]) => {
+      t.is ( Hex.output ( Color.parse ( input ) ), result )
+    });
+
+  });
+
+  it ( 'parses RGB with commas', t => {
+
+    const tests = [
+      ['rgb(10%, 20%, 30%)', '#1a334d'],
+      ['rgb(1 , 20 , 255)', '#0114ff'],
+      ['rgb(1,20,255)', '#0114ff'],
+      ['rgb( 1,20,255 )', '#0114ff'],
+    ];
+
+    tests.forEach( ([ input, result ]) => {
+      t.is ( Hex.output ( Color.parse ( input ) ), result )
+    });
+
+  });
+
+  it ( 'parses RGB without commas', t => {
+
+    const tests = [
+      ['rgb(10% 20% 30%)', '#1a334d'],
+      ['rgb(1 20 255)', '#0114ff'],
+      ['rgb(   1    20     255   )', '#0114ff'],
+    ];
+
+    tests.forEach( ([ input, result ]) => {
+      t.is ( Hex.output ( Color.parse ( input ) ), result )
+    });
+
+  });
+
+  it ( 'parses RGB with mixed units', t => {
+
+    const tests = [
+      ['rgb(10% 20 30%)', '#1a144d'],
+      ['rgb(1 20 25.5)', '#01141a'],
+    ];
+
+    tests.forEach( ([ input, result ]) => {
+      t.is ( Hex.output ( Color.parse ( input ) ), result )
+    });
+
+  });
+
+
+
+  it ( 'throws an error when parsing malformed RGB input', t => {
+
+    const tests = [
+      'rgb(1,, 20, 255)',
+      'rgb(1%,, 20%, 255%)',
+      'rgb(1%, 255%)',
+      'rgb(1)',
+      'rgb()',
+      'rgb(1, 20, 255',
+      'rgb 1, 20, 255',
+      'rgb 1, 20, 255)',
+      'rgb(1, a, 255)',
+    ];
+
+    tests.forEach( color => {
+      t.throws ( () => Color.parse ( color ), `Unsupported color format: "${color}"` );
     });
 
   });
