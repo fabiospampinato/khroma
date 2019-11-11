c3df1a0839763565a0028a788c551c6949249dda
Andrew P Maney <amaney@usc.edu>
2019-11-11 11:10:23 -0800

    Adds Hex support

--- src/color/hex.ts
+++ src/color/hex.ts
@@ -9,7 +9,7 @@ import Abstract from './abstract';
 
 class Hex extends Abstract {
 
-  re = /#([a-f0-9]{2})([a-f0-9]{2})([a-f0-9]{2})/i; //TODO: Support all possible formats
+  re = /#((?:[a-f0-9]{1}){3,4}|(?:[a-f0-9]{2}){3,4})$/i
 
   parse ( color: string ): RGBA | undefined {
 
@@ -17,11 +17,36 @@ class Hex extends Abstract {
 
     if ( !match ) return;
 
+    /* 
+     * Alright so the following code looks pretty weird, but allow me to explain.
+     * I am doing this to avoid a nasty regex. Hex colors can be 3, 4, 6, or 8-digit.
+     * It is pretty difficult to write a regex that cleanly matches all of these formats and captures the channels.
+     * What I have done instead is write a regex that will match all valid formats without capturing groups.
+     * Then, I use this loop to split up the hex code into the appropriate channels.
+     * I know the hex code will be in one of these forms: RGB, RGBA, RRGGBB, RRGGBBAA
+     */
+
+    const hex = match[1];
+
+    const colors: string[] = [];
+
+    for ( let i = 0; i < hex.length; i += Math.floor ( hex.length / 3 ) ) {
+
+      colors.push ( hex.slice ( i, i + Math.floor ( hex.length / 3 ) ) )
+
+    }
+
+    const [ r, g, b, a ] = colors;
+
+    const formatColor = ( color: string ): number => Utils.hex2dec ( Utils.padStart ( color, color, 2 ) )
+
+    const formatAlpha = ( alpha: string ): number => Utils.hex2frac ( Utils.padStart ( alpha, alpha, 2 ) );
+
     return {
-      r: Utils.hex2dec ( match[1] ),
-      g: Utils.hex2dec ( match[2] ),
-      b: Utils.hex2dec ( match[3] ),
-      a: 1
+      r: formatColor ( r ),
+      g: formatColor ( g ),
+      b: formatColor ( b ),
+      a: formatAlpha ( a || 'ff' )
     };
 
   }
@@ -30,7 +55,7 @@ class Hex extends Abstract {
 
     if ( rgba.a < 1 ) { // #RRGGBBAA
 
-      return `#${Utils.dec2hex ( rgba.r )}${Utils.dec2hex ( rgba.g )}${Utils.dec2hex ( rgba.b )}${Utils.per2hex ( rgba.a )}`;
+      return `#${Utils.dec2hex ( rgba.r )}${Utils.dec2hex ( rgba.g )}${Utils.dec2hex ( rgba.b )}${Utils.frac2hex ( rgba.a )}`;
 
     } else { // #RRGGBB
 
